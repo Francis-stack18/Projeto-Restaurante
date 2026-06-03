74343045bef322ac2c97033b3be1dcfb0e36a03c
Francis <francis13322@gmail.com>
2026-06-03 15:01:46 -0300

    MY02 - Criando a estrutura do projeto

--- saboroso/app.js
+++ saboroso/app.js
@@ -9,7 +9,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+//
+// View Engine Setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
