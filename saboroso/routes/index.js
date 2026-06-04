d22ce1a7e3e8059b6c49a7b2dad4b51e3ced22c0
Francis <francis13322@gmail.com>
2026-06-03 22:31:53 -0300

    MY09 - Organizando o Conteudo com Includes

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -1,13 +1,11 @@
 var conn = require("../inc/db");
 var express = require("express");
 var router = express.Router();
+var menus = require("../inc/menus");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  conn.query("SELECT * FROM tb_menus ORDER BY title", (err, results) => {
-    if (err) {
-      console.log(err);
-    }
+  menus.getMenus().then((results) => {
     res.render("index", { title: "Restaurante Saboroso!", menus: results });
   });
 });
@@ -21,10 +19,13 @@ router.get("/contacts", function (req, res, next) {
 });
 
 router.get("/menus", function (req, res, next) {
-  res.render("menus", {
-    title: "Menus - Restaurante Saboroso!",
-    background: "images/img_bg_1.jpg",
-    h1: "Saboreie nosso menu",
+  menus.getMenus().then((results) => {
+    res.render("menus", {
+      title: "Menus - Restaurante Saboroso!",
+      background: "images/img_bg_1.jpg",
+      h1: "Saboreie nosso menu",
+      menus: results,
+    });
   });
 });
 
