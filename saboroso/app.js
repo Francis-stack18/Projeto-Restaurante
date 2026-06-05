a2472368f699520058da37f9dddd79f430fa506a
Francis <francis13322@gmail.com>
2026-06-04 21:37:06 -0300

    MY15 - Efetuando login na Administracao

--- saboroso/app.js
+++ saboroso/app.js
@@ -5,7 +5,6 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 require("dotenv").config();
 
-// 1. Importa a Sessão e o Connect Redis (padrão v6)
 var session = require("express-session");
 var RedisStore = require("connect-redis")(session);
 var redis = require("redis");
@@ -15,17 +14,14 @@ var adminRouter = require("./routes/admin");
 
 var app = express();
 
-// 2. Cria o cliente clássico do Redis compatível com a V3.2
 var redisClient = redis.createClient({
   host: "127.0.0.1",
   port: 6379,
 });
 
-// View Engine Setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-// 3. Configura a sessão
 app.use(
   session({
     store: new RedisStore({
@@ -46,14 +42,10 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/admin", adminRouter);
 
-// ... (Mantenha o resto das rotas de erro no final exatamente iguais)
-
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handlers
 app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
--- saboroso/routes/admin.js
+++ saboroso/routes/admin.js
@@ -1,15 +1,32 @@
 var express = require("express");
 var router = express.Router();
+var users = require("../inc/users");
 
 router.get("/", function (req, res, next) {
   res.render("admin/index", {});
 });
 
 router.get("/login", function (req, res, next) {
-  if (!req.session.views) req.session.views = 0;
+  users.render(req, res, null);
+});
 
-  console.log("SESSION:", req.session.views++);
-  res.render("admin/login", {});
+router.post("/login", function (req, res, next) {
+  if (!req.body.email) {
+    users.render(req, res, "Preencha o campo e-mail.");
+  } else if (!req.body.password) {
+    users.render(req, res, "Preencha o campo senha.");
+  } else {
+    users
+      .login(req.body.email, req.body.password)
+      .then((user) => {
+        req.session.user = user;
+
+        res.redirect("/admin");
+      })
+      .catch((err) => {
+        users.render(req, res, err.message || err);
+      });
+  }
 });
 
 router.get("/contacts", function (req, res, next) {
