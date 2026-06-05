b62623dcfd529a28514696e7620ec5824847392e
Francis <francis13322@gmail.com>
2026-06-04 21:11:17 -0300

    MY14 - Express Session com Redis

--- saboroso/app.js
+++ saboroso/app.js
@@ -5,14 +5,37 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 require("dotenv").config();
 
+// 1. Importa a Sessão e o Connect Redis (padrão v6)
+var session = require("express-session");
+var RedisStore = require("connect-redis")(session);
+var redis = require("redis");
+
 var indexRouter = require("./routes/index");
 var adminRouter = require("./routes/admin");
 
 var app = express();
 
+// 2. Cria o cliente clássico do Redis compatível com a V3.2
+var redisClient = redis.createClient({
+  host: "127.0.0.1",
+  port: 6379,
+});
+
 // View Engine Setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+// 3. Configura a sessão
+app.use(
+  session({
+    store: new RedisStore({
+      client: redisClient,
+    }),
+    secret: "Password",
+    resave: false,
+    saveUninitialized: true,
+  }),
+);
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -23,6 +46,8 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/admin", adminRouter);
 
+// ... (Mantenha o resto das rotas de erro no final exatamente iguais)
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
@@ -30,11 +55,8 @@ app.use(function (req, res, next) {
 
 // error handlers
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
