4bf0661616f4ec5069cda2d29bce50ef015f497a
Francis <francis13322@gmail.com>
2026-06-08 08:37:47 -0300

    MY28 - Administracao do Menu - Alterando dados via POST

--- saboroso/inc/menus.js
+++ saboroso/inc/menus.js
@@ -20,22 +20,65 @@ module.exports = {
   },
   save(fields, files) {
     return new Promise((resolve, reject) => {
-      fields.photo = `images/${path.parse(files.photo.path).base}`;
+      if (files && files.photo && files.photo.size > 0) {
+        fields.photo = `images/${path.parse(files.photo.path).base}`;
+      } else {
+        delete fields.photo;
+      }
 
-      conn.query(
-        `
-          INSERT INTO tb_menus (title, description, price, photo)
-          VALUES(?, ?, ?, ?)
-        `,
-        [fields.title, fields.description, fields.price, fields.photo],
-        (err, results) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(results);
-          }
-        },
-      );
+      let query, queryParams;
+
+      if (parseInt(fields.id) > 0) {
+        if (fields.photo) {
+          query = `
+                        UPDATE tb_menus 
+                        SET title = ?, description = ?, price = ?, photo = ?
+                        WHERE id = ?
+                    `;
+          queryParams = [
+            fields.title,
+            fields.description,
+            fields.price,
+            fields.photo,
+            fields.id,
+          ];
+        } else {
+          query = `
+                        UPDATE tb_menus 
+                        SET title = ?, description = ?, price = ?
+                        WHERE id = ?
+                    `;
+          queryParams = [
+            fields.title,
+            fields.description,
+            fields.price,
+            fields.id,
+          ];
+        }
+      } else {
+        if (!fields.photo) {
+          fields.photo = "images/boxed-bg.jpg";
+        }
+
+        query = `
+                    INSERT INTO tb_menus (title, description, price, photo)
+                    VALUES(?, ?, ?, ?)
+                `;
+        queryParams = [
+          fields.title,
+          fields.description,
+          fields.price,
+          fields.photo,
+        ];
+      }
+
+      conn.query(query, queryParams, (err, results) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results);
+        }
+      });
     });
   },
 };
