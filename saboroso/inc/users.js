c0d768b02d8c4df3b1520f7d0d05d3b7eca143cb
Francis <francis13322@gmail.com>
2026-06-09 13:51:15 -0300

    MY40 - Administracao dos usuarios criando telas

--- saboroso/inc/users.js
+++ saboroso/inc/users.js
@@ -38,7 +38,7 @@ module.exports = {
     return new Promise((resolve, reject) => {
       conn.query(
         `
-                SELECT * FROM tb_users ORDER BY title
+                SELECT * FROM tb_users ORDER BY name
             `,
         (err, results) => {
           if (err) {
@@ -62,19 +62,18 @@ module.exports = {
         query = `
                     UPDATE tb_users
                     SET name = ?,
-                        email = ?,
+                        email = ?
                     WHERE id = ?    
                 `;
       } else {
         query = `
-                    INSERT INTO tb_users (name, emai, password)
+                    INSERT INTO tb_users (name, email, password)
                     VALUES(?, ?, ?)
                 `;
 
         params.push(fields.password);
       }
-
-      conn.query(query, queryParams, (err, results) => {
+      conn.query(query, params, (err, results) => {
         if (err) {
           reject(err);
         } else {
