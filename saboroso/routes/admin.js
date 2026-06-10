7ccde556265bd3a8cd967a5a4f1de5a68bb376ff
Francis <francis13322@gmail.com>
2026-06-10 12:23:32 -0300

    MY43 - Administracao dos emails recebidos

--- saboroso/routes/admin.js
+++ saboroso/routes/admin.js
@@ -6,6 +6,7 @@ var menus = require("../inc/menus");
 var reservations = require("../inc/reservations");
 var moment = require("moment");
 var contacts = require("../inc/contacts");
+var emails = require("../inc/emails");
 
 moment.locale("pt-BR");
 
@@ -92,7 +93,25 @@ router.delete("/contacts/:id", function (req, res, next) {
 });
 
 router.get("/emails", function (req, res, next) {
-  res.render("admin/emails", admin.getParams(req));
+  emails.getEmails().then((data) => {
+    res.render(
+      "admin/emails",
+      admin.getParams(req, {
+        data,
+      }),
+    );
+  });
+});
+
+router.delete("/emails/:id", function (req, res, next) {
+  emails
+    .delete(req.params.id)
+    .then((results) => {
+      res.send(results);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 });
 
 router.get("/menus", function (req, res, next) {
--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var menus = require("../inc/menus");
 var reservations = require("../inc/reservations");
 var contacts = require("../inc/contacts");
+var emails = require("../inc/emails");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -89,4 +90,14 @@ router.get("/services", function (req, res, next) {
   });
 });
 
+router.post("/subscribe", function (req, res, next) {
+  emails
+    .save(req)
+    .then((results) => {
+      res.send(results);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
 module.exports = router;
