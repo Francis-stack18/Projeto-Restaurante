a10eb412804d736cb146954a45287b6a09d0762e
Francis <francis13322@gmail.com>
2026-06-10 22:14:24 -0300

    MY48 - Obtendo dados do servidor e atualizando grafico

--- saboroso/inc/reservations.js
+++ saboroso/inc/reservations.js
@@ -1,5 +1,6 @@
 var conn = require("./db");
 var Pagination = require("../inc/Pagination");
+let moment = require("moment");
 
 module.exports = {
   render(req, res, error, success) {
@@ -100,6 +101,42 @@ module.exports = {
             reject(err);
           } else {
             resolve(results);
+          }
+        },
+      );
+    });
+  },
+
+  chart(req) {
+    return new Promise((resolve, reject) => {
+      conn.query(
+        `
+                SELECT
+                    DATE_FORMAT(date, '%Y-%m') AS date,
+                    COUNT(*) AS total,
+                    SUM(people) / COUNT(*) AS avg_people
+                FROM tb_reservations
+                WHERE
+                    date BETWEEN ? AND ?
+                GROUP BY DATE_FORMAT(date, '%Y-%m')
+                ORDER BY DATE_FORMAT(date, '%Y-%m') DESC;
+            `,
+        [req.query.start, req.query.end],
+        (err, results) => {
+          if (err) {
+            reject(err);
+          } else {
+            let months = [];
+            let values = [];
+
+            results.forEach((row) => {
+              months.push(moment(row.date).format("MMM YYYY"));
+              values.push(row.total);
+            });
+            resolve({
+              months,
+              values,
+            });
           }
         },
       );
